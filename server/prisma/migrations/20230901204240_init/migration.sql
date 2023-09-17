fe630c9a334d2b1f0f985c7e24c29d9377d672ce
deaving-71 <110570275+deaving-71@users.noreply.github.com>
2023-09-17 20:31:13 +0100

    Delete server/prisma/migrations directory

--- server/prisma/migrations/20230901204240_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- CreateIndex
-CREATE INDEX "users_username_idx" ON "users"("username");
--- server/prisma/migrations/20230910222651_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "avatar" SET DEFAULT '/assets/avatar_male.png';
